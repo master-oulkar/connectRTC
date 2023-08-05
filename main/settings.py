b0281c03175f226bfd2f233ee6a87e9dbf75e722
master-oulkar <141329099+master-oulkar@users.noreply.github.com>
2023-08-05 10:11:34 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -90,7 +90,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [f"{r}"], 
+            "hosts": [redis.from_url(env('REDIS_URL'))], 
         },
     },
 }
