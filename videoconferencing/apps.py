ec604309b9ab1882bfc96a01d3c2c6f3aa60db77
master-oulkar <141329099+master-oulkar@users.noreply.github.com>
2023-08-08 17:15:39 +0530

    Delete videoconferencing directory

--- videoconferencing/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VideoconferencingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'videoconferencing'
--- videoconferencing/routing.py
+++
@@ -1,8 +0,0 @@
-# chat/routing.py
-from django.urls import re_path
-
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r"ws/videocall/(?P<channel_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
-]
