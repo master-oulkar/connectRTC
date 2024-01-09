8d2ec49d8f7598c5d5f8f55d3de4319aff66fe26
shubham oulkar <91728992+ShubhamOulkar@users.noreply.github.com>
2024-01-09 13:47:49 +0530

    settings.py     'videoconferencing.apps.',

--- main/settings.py
+++ main/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'videoconferencing',
+    'videoconferencing.apps.',
+    'videoconferencing.apps.VideoconferencingConfig',
 ]
 
 MIDDLEWARE = [
