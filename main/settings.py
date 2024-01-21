4f5f45833fa6df42e7b16b438c121ad48b7d8f46
master-oulkar <141329099+master-oulkar@users.noreply.github.com>
2024-01-21 16:20:54 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'videoconferencing.apps.',
     'videoconferencing.apps.VideoconferencingConfig',
 ]
 
